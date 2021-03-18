100571606244c1c9d326887031f56b0bf55dcf39
Henry Widd <henryw374@gmail.com>
2021-03-18 10:09:20 +0000

    fix some var-args macros

--- src/com/widdindustries/log4j2/log_api.clj
+++ src/com/widdindustries/log4j2/log_api.clj
@@ -1,7 +1,7 @@
 (ns com.widdindustries.log4j2.log-api
   (:require [com.widdindustries.log4j2.log-impl :as impl])
   (:import [org.apache.logging.log4j.message Message MapMessage]
-           [org.apache.logging.log4j Logger LogBuilder Level LogManager Marker]
+           [org.apache.logging.log4j LogBuilder Level Marker]
            [org.apache.logging.log4j.util Supplier]
            (java.util Map)))
 
@@ -21,13 +21,11 @@
      (instance? Message thing) (.log builder ^Message thing)
      (instance? Map thing) (.log builder ^Message (MapMessage. ^Map thing))
      (instance? Supplier thing) (.log builder ^Supplier thing)
-
      :else (.log builder ^Object thing)))
-  ([^LogBuilder builder ^String thing [x & _xs :as args]]
-   (println args)
-   (if (instance? Supplier x)
-     (.log builder ^String thing #^"[Lorg.apache.logging.log4j.util.Supplier;" (into-array Supplier args))
-     (.log builder ^String thing #^"[Ljava.lang.Object;" (into-array Object args)))))
+  ([^LogBuilder builder ^String thing varargs]
+   (if (instance? Supplier (first varargs))
+     (.log builder ^String thing #^"[Lorg.apache.logging.log4j.util.Supplier;" (into-array Supplier varargs))
+     (.log builder ^String thing #^"[Ljava.lang.Object;" (into-array Object varargs)))))
 
 (defn with-location [^LogBuilder builder]
   (.withLocation builder))
@@ -40,15 +38,15 @@
 
 (defmacro fatal 
   ([thing] `(log (impl/log-builder Level/FATAL) ~thing))
-  ([thing & more] `(log (impl/log-builder Level/FATAL) ~thing ~more)))
+  ([thing & more] `(log (impl/log-builder Level/FATAL) ~thing [~@more])))
 
 (defmacro error 
   ([thing] `(log (impl/log-builder Level/ERROR) ~thing))
-  ([thing & more] `(log (impl/log-builder Level/ERROR) ~thing ~more)))
+  ([thing & more] `(log (impl/log-builder Level/ERROR) ~thing [~@more])))
 
 (defmacro warn 
   ([thing] `(log (impl/log-builder Level/WARN) ~thing))
-  ([thing & more] `(log (impl/log-builder Level/WARN) ~thing ~more)))
+  ([thing & more] `(log (impl/log-builder Level/WARN) ~thing [~@more])))
 
 (defmacro info 
   ([thing] `(log (impl/log-builder Level/INFO) ~thing))
@@ -61,20 +59,3 @@
 (defmacro trace 
   ([thing] `(log (impl/log-builder Level/TRACE) ~thing))
   ([thing & more] `(log (impl/log-builder Level/TRACE) ~thing [~@more])))
-
-
-(comment
-   
-  (info "hello")
-  (info "hello {}" :foo)
-  (-> (info-builder)
-      (with-location)
-      (with-throwable *e)
-      (log "foo"))
-
-
-  (macroexpand '(info "hello {}" :foo))
-
-  )
-
-
